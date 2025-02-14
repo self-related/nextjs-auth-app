c409f6a8542c24101b3846646d5cc05e92abd76d
Nekit <nick.misiura@yandex.ru>
2025-02-14 11:38:27 +0300

    1) init Nest.js app WITHOUT TailwindCSS; 2) init a newer version of TailwindCSS manually

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="text-2xl font-bold p-5">Next Auth</div>
+  );
+}
