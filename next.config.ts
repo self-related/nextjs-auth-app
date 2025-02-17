51d27ae9da2fae2a1bbc4dc3a39f60559bada2a6
Nekit <nick.misiura@yandex.ru>
2025-02-17 12:27:54 +0300

    allow images from any *.googleusercontent.com domain by hostname pattern

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: [`avatars.githubusercontent.com`],
+    remotePatterns: [
+        {
+            hostname: '*.googleusercontent.com'
+        }
+    ]
   }
 };
 
